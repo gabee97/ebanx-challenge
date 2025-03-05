e653613392fac467e93459077a8cb1851179ac56
Gabriel <gaabriel123@hotmail.com>
2025-03-05 10:48:17 -0300

    Criada execao para rotas sem autenticacao

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,11 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/reset',
+        '/balance',
+        '/event',
+        '/api/reset',
+        '/api/balance',
+        '/api/event'
     ];
 }
